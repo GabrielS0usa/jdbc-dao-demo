86770aec45a12b53966a03d885af9596354e0c7c
Gabriel_Sousa <gabriel.sist.inf@gmail.com>
2024-09-12 14:45:41 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.text.ParseException;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 	
 		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(10, "Gabriel", "gabriel@gmail.com", new Date(), 1500.00, obj);
 	
-		System.out.println(obj);
+		System.out.println(seller);
 	
 	}
 }
